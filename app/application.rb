ff86cb89a0c846f9d8e41b00c847ab1dce53f1b2
Michael Roose <mjroose@gmail.com>
2018-10-14 16:04:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
       end
     elsif req.path.match(/add/)
       search_term = req.params["q"]
-      if @@items.include? search_term
+      if @@items.includes? search_term
         @@cart << search_term
         resp.write("added #{search_term}")
       else
