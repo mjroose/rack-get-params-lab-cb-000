98255bceefa0321ab1f2adc8eac5fd33e88da2c5
Michael Roose <mjroose@gmail.com>
2018-10-14 16:04:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,9 @@ class Application
       search_term = req.params["q"]
       if @@items.include? search_term
         @@cart << search_term
+        resp.write("added #{search_term}")
+      else
+        resp.write("We don't have that item")
       end
     else
       resp.write "Path Not Found"
